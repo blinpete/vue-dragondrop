b4c9e486c8fd31573aa85bb7b85d09d8da9578fb
Peter <pehrwillhelm@hotmail.com>
2021-02-18 20:38:14 +0300

    [new] utils started: vendor prefixes and css staff from vue-slicksort, and my own eventManager

--- _utils.js
+++ _utils.js
@@ -5,6 +5,8 @@ const eventManager = {
     move: ['touchmove', 'mousemove'],
     end: ['touchend', 'touchcancel', 'mouseup'],
 
+    hover: ['mouseenter'],
+
     addListeners(el, events){
         for (const key in events) {
             this[key].forEach(name => {
@@ -21,5 +23,53 @@ const eventManager = {
         }
     },
 };
+
+function getCSSPixelValue(stringValue) {
+    if (stringValue.substr(-2) === 'px') {
+        return parseFloat(stringValue);
+    }
+    return 0;
+}
+
+function getElementMargin(element) {
+    const style = window.getComputedStyle(element);
+
+    return {
+        top: getCSSPixelValue(style.marginTop),
+        right: getCSSPixelValue(style.marginRight),
+        bottom: getCSSPixelValue(style.marginBottom),
+        left: getCSSPixelValue(style.marginLeft),
+    };
+}
+
+function getElementMeasures(node){
+    const rect = node.getBoundingClientRect();
+    const margins = getElementMargin(node);
+    return {
+      width: node.offsetWidth+'px',
+      height: node.offsetHeight+'px',
+      top: rect.top - margins.top+'px',
+      left: rect.left - margins.left+'px',
+    }
+}
+
+
+
+const vendorPrefix = (function() {
+    if (typeof window === 'undefined' || typeof document === 'undefined') return ''; // server environment
+    // fix for:
+    //    https://bugzilla.mozilla.org/show_bug.cgi?id=548397
+    //    window.getComputedStyle() returns null inside an iframe with display: none
+    // in this case return an array with a fake mozilla style in it.
+    const styles = window.getComputedStyle(document.documentElement, '') || ['-moz-hidden-iframe'];
+    const pre = (Array.prototype.slice.call(styles).join('').match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o']))[1];
+  
+    switch (pre) {
+      case 'ms':
+        return 'ms';
+      default:
+        return pre && pre.length ? pre[0].toUpperCase() + pre.substr(1) : '';
+    }
+  })();
 
 
