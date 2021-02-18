8b658bf54e2bd0dfe21a28d78e2314db2b4eb8d3
Peter <pehrwillhelm@hotmail.com>
2021-02-18 20:36:08 +0300

    [new] element mixin started: original logic in onHover

--- _element.js
+++ _element.js
@@ -5,15 +5,49 @@
 const ElementMixin = {
   // inject: ['manager'],
   props: {
-    
+    index: Number,
+  },
+
+  data(){
+    return {
+      manager: manager,
+      events: {
+        hover: this.onHover
+      },
+    };
   },
 
   mounted(){
+    eventManager.addListeners(this.$el, this.events);
   },
 
   beforeDestroy(){
+    eventManager.removeListeners(this.$el, this.events);
   },
 
   methods: {
+    onHover(e){
+      console.log("[onHover] item.id: ", this.index);
+
+      if (this.manager.dragging) {
+        console.log("[onHover] adding ::before element");
+        console.log("[onHover] classList: ",this.manager.node.classList);
+
+        if (this.$parent.groupName !== this.manager.groupName) {
+          return;
+        }
+
+        const hs = this.manager.helper.style;
+        const preview = this.manager.previewNode.node;
+
+        preview.style.width  = this.$el.offsetWidth+'px'; 
+        preview.style.height = hs.width*hs.height/preview.st
+        this.$parent.$el.insertBefore(preview, this.$el);
+      }
+
+      this.manager.activeIndex = this.index;
+      this.manager.node = this.$el;
+
+    },
   },
 };
